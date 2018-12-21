74e78ddf540ea026222361340dcd0ffb836882db
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-21 12:27:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+  return array.join(" and ") if array.length == 2
+  return nil if array.nil?
+  return array[0] if array.length == 1
+  return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 2
 end
+
